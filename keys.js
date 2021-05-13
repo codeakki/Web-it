0d0573bb993db7392b127a7c44f4eff847215952
codeakki <codewithakki@gmail.com>
2021-05-13 20:11:03 +0530

    initial

---
+++ keys.js
@@ -0,0 +1,11 @@
+// API keys  https://developers.google.com/custom-search/v1/using_rest
+
+//API key = AIzaSyBl6wVtjmUj77K-XQH6eqJUh417aoJKFTo
+
+export const API_KEY = 'AIzaSyBl6wVtjmUj77K-XQH6eqJUh417aoJKFTo';
+
+// Context Key
+export const CONTEXT_KEY = 'ff09a6de4733fe70b';
+
+/*  <script async src="https://cse.google.com/cse.js?cx=ff09a6de4733fe70b"></script>
+<div class="gcse-search"></div>*/
